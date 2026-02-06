21bee73c31ae5b93b8eff65ff337ce5f2ec73bb7
theygrewv <ptsdhippie@gmail.com>
2026-02-05 21:23:58 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,38 @@
-const CACHE_NAME = 'bluetune-cache-v1';
-// This version uses relative paths (./) to find your files regardless of the URL
+const CACHE_NAME = 'bluetune-v1';
+
+// The Master Assets List
 const ASSETS = [
   './',
   'index.html',
-  'index.min.js',
-  'manifest.json'
+  'library.js',
+  'manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/3658/3658959.png'
 ];
 
+// Install Event: Saves everything to your phone's memory
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      // Adding assets one by one so one failure doesn't break the whole thing
+      // Using allSettled so one failed icon doesn't break the whole app
       return Promise.allSettled(
         ASSETS.map(asset => cache.add(asset))
+      );
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// Activate Event: Cleans up old versions of the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
     })
   );
 });
 
+// Fetch Event: Serves the app from memory for speed and security
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
